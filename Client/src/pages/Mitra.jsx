2f727bb8c130e16a265fc2d26dc223e7fe0cc492
oktapiani <ramadhanioktapiani@gmail.com>
2025-09-10 21:05:29 +0700

    add img for mitra

--- Client/src/pages/Mitra.jsx
+++ Client/src/pages/Mitra.jsx
@@ -1,4 +1,8 @@
 import ImgPro from "../assets/img-kemnaker.png";
+import ImgMitra2 from "../assets/img-cimb-mitra.png";
+import ImgMitra3 from "../assets/img-edelweis.jpeg";
+import ImgMitra4 from "../assets/img-ubraw.jpeg";
+
 
 export default function Mitra() {
   return (
@@ -19,10 +23,10 @@ export default function Mitra() {
           </div>
 
           {/* Grid Container */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {/* Card 1 */}
             <div className="max-w-sm bg-white rounded-lg shadow-sm dark:bg-white-800 dark:border-gray-700 hover:shadow-xl transform hover:-translate-y-0.5">
-              <img className="rounded-t-lg" src={ImgPro} alt="" />
+              <img className="w-full h-40 object-contain rounded-t-lg" src={ImgPro} alt="" />
 
               <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
@@ -59,7 +63,7 @@ export default function Mitra() {
 
             {/* Card 2 */}
             <div className="max-w-sm bg-white rounded-lg shadow-sm dark:bg-white-800 dark:border-gray-700 hover:shadow-xl transform hover:-translate-y-0.5 ">
-              <img className="rounded-t-lg" src={ImgPro} alt="" />
+              <img className="w-full h-40 object-contain rounded-t-lg" src={ImgMitra2} alt="" />
 
               <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
@@ -95,7 +99,43 @@ export default function Mitra() {
             </div>
 
             <div class="max-w-sm bg-white rounded-lg shadow-sm dark:bg-white-800 dark:border-gray-700 hover:shadow-xl transform hover:-translate-y-0.5">
-              <img class="rounded-t-lg" src={ImgPro} alt="" />
+              <img class="w-full h-40 object-contain rounded-t-lg" src={ImgMitra3} alt="" />
+
+              <div class="p-5">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+                  Noteworthy technology acquisitions 2021
+                </h5>
+
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-700">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <a
+                  href="/detail-program"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg bg-gradient-to-r from-teal-500 to-blue-500 hover:from-blue-600 hover:to-teal-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Read more
+                  <svg
+                    class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+             <div class="max-w-sm bg-white rounded-lg shadow-sm dark:bg-white-800 dark:border-gray-700 hover:shadow-xl transform hover:-translate-y-0.5">
+              <img class="w-full h-40 object-contain rounded-t-lg" src={ImgMitra4} alt="" />
 
               <div class="p-5">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
