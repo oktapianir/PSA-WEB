ac6cb363bc3068f41c40748b90abacc09a4a8c3c
oktapiani <ramadhanioktapiani@gmail.com>
2025-09-11 14:14:40 +0700

    change size for title

--- Client/src/pages/Admin/DashboardAdmin.jsx
+++ Client/src/pages/Admin/DashboardAdmin.jsx
@@ -16,7 +16,7 @@ export default function DashboardAdmin() {
   }, []);
   return (
     <div className="p-8">
-      <h1 className="text-2xl font-bold text-black">Selamat Datang, Admin!</h1>
+      <h1 className="text-5xl font-bold text-black">Selamat Datang, Admin!</h1>
       <p className="mt-4 text-gray-600">Ini adalah halaman dashboard admin.</p>
 
       {/* Card Total Users */}
